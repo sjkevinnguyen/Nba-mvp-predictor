36a0d5953c126c769f84f21670e252dfc1972e06
Kevin Nguyen <sjkevinnguyen@gmail.com>
2025-04-14 22:52:54 -0700

    Fix webscrape.py bad merging, start on model.ipynb
    Original webscrape.py was creating extra columns and wrong NaN values.
    Adjusted column names before merging and removed certain columns.
    Changed output to only one large df for easier model use.
    Started on model.ipynb.

--- webscrape.py
+++ webscrape.py
@@ -4,68 +4,65 @@
 
 # Necessary imports
 import os
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
-
 
 # Basic Variables
 years = range(2004, 2025)
+url1 = 'https://www.basketball-reference.com/awards/awards_{}.html'
+url2 = 'https://www.basketball-reference.com/leagues/NBA_{}_totals.html'
+url3 = 'https://www.basketball-reference.com/leagues/NBA_{}_per_game.html'
+url4 = 'https://www.basketball-reference.com/leagues/NBA_{}_advanced.html'
 
-url1 = 'https://www.basketball-reference.com/leagues/NBA_{}_totals.html'
-url2 = 'https://www.basketball-reference.com/leagues/NBA_{}_advanced.html'
-url3 = 'https://www.basketball-reference.com/awards/awards_{}.html'
-url4 = 'https://www.basketball-reference.com/leagues/NBA_{}_per_game.html'
+# Webscrape the data and condense all the seasons' data into one DataFrame
 
-
-
-# Webscrape NBA data, convert to csv, and place in appropriate folder
-
-try:
-    os.mkdir('data')
-except FileExistsError:
-    pass
+df = pd.DataFrame()
 
 for year in years:
+
+    # Webscrape each link for the stats table
+    mvp = pd.read_html(url1.format(year), header=1, attrs={'id':'mvp'})[0]
+    totals = pd.read_html(url2.format(year), header=0, attrs={'id':'totals_stats'})[0]
+    per_game = pd.read_html(url3.format(year), header=0, attrs={'id':'per_game_stats'})[0]
+    advanced = pd.read_html(url4.format(year), header=0, attrs={'id':'advanced'})[0]
+
+
+    # Edit the features before combining
+    mvp_ = mvp.drop(columns=['Rank', 'Age', 'Tm', 'First', 'Pts Won', 'Pts Max'], axis=1).\
+        rename(columns={'MP':'MPPG', 'PTS':'PTSPG', 'TRB':'TRBPG', 'AST':'ASTPG', 'STL':'STLPG', 'BLK':'BLKPG'}).\
+        fillna(0)
     
-    # Webscrape data
-    total = pd.read_html(url1.format(year), header=0, attrs={'id':'totals_stats'})[0]
-    advanced = pd.read_html(url2.format(year), header=0, attrs={'id':'advanced'})[0]
-    mvp = pd.read_html(url3.format(year), header=1, attrs={'id':'mvp'})[0]
-    per_game = pd.read_html(url4.format(year), header=0, attrs={'id':'per_game_stats'})[0]
+    totals_ = totals.drop(['Rk', 'Age', 'Team', 'Pos', 'Awards'], axis=1).\
+                drop_duplicates(subset=['Player'], keep='first', ignore_index=True).\
+                iloc[:-1].fillna(0)
 
-    # Adjust DataFrames
-    total = total.drop(['Rk', 'Age', 'Team', 'Pos', 'Awards'], axis=1).\
-                iloc[:-1].\
-                drop_duplicates(subset=['Player'], keep='first', ignore_index=True)
-    advanced = advanced.drop(['Rk', 'Age', 'Team', 'Pos', 'Awards'], axis=1).\
-                iloc[:-1].\
-                drop_duplicates(subset=['Player'], keep='first', ignore_index=True)
+    per_game_ = per_game.drop(['Rk', 'Age', 'Team', 'Pos', 'Awards'], axis=1).\
+                    drop_duplicates(subset=['Player'], keep='first', ignore_index=True).\
+                    rename(columns={'MP':'MPPG', 'FG':'FGPG', 'FGA':'FGAPG', '3P':'3PPG', '3PA':'3PAPG','2P':'2PPG', '2PA':'2PAPG', 'FT':'FTPG', 'FTA':'FTAPG',
+                                    'ORB':'ORBPG', 'DRB':'DRBPG', 'TRB':'TRBPG', 'AST':'ASTPG', 'STL':'STLPG', 'BLK':'BLKPG', 'TOV':'TOVPG',
+                                    'PF':'PFPG', 'PTS':'PTSPG'}).\
+                    fillna(0).iloc[:-1]
     
-    per_game = per_game.drop(['Rk', 'Team', 'Age','Pos', 'Awards'], axis=1).\
-                iloc[:-1].\
-                drop_duplicates(subset=['Player'], keep='first', ignore_index=True)
+    advanced_ = advanced.drop(['Rk', 'Age', 'Team', 'Pos', 'Awards'], axis=1).\
+                    drop_duplicates(subset=['Player'], keep='first', ignore_index=True).\
+                    fillna(0).iloc[:-1]
+    
+    # Merge the DataFrames together and add the year
+    f = pd.merge(totals_, per_game_, how='outer')
+    f = pd.merge(f, advanced_, how='outer')
+    f = pd.merge(f, mvp_, how='outer').fillna(0)
+    f['Year'] = year
 
-    mvp = mvp.drop(['Rank', 'Age', 'Tm'], axis=1)
+    # Concatenate the DataFrame to the previous one vertically
+    if df.empty:
+        df = f
+    else:
+        df = pd.concat([df, f])
+    
+    # Print the progress along the way
+    print(f'Season {year-1}-{year} DataFrame has been completed')
 
-    #Adjust per game columns to match mvp columns (b/c they were mislabeled on basketball reference)
-    per_game = per_game.add_suffix('PG').drop(columns=['GPG', 'GSPG'], axis=1).rename(columns={'PlayerPG':'Player'})
-
-    # Drop mvp columns instead of changing column names b/c I'm lazy
-    mvp.drop(columns=['MP','PTS','TRB','AST','STL','BLK','FG%','3P%','FT%'])
-
-    # Combine Total + Advanced and Per_Game + mvp DataFrames
-    total_advanced = pd.merge(total, advanced, how='outer')
-    per_game_mvp = pd.merge(per_game, mvp, how='outer')
-
-    # Merge everything into the final DataFrame, Add First Point Share, and Year
-    final = pd.merge(per_game_mvp, total_advanced, how='outer', on='Player').fillna(0)
-    final['Pts Max'] = final['Pts Max'].max()
-    final['First Pts Share'] = final['First'] / final['Pts Max']
-    final['Year'] = year
-
-    file_name = '{}.csv'
-    final.to_csv(os.path.join('data/',file_name.format(year)))
+# Save the entire DataFrame to csv file
+df.to_csv('data.csv', index = False)
 
 
 
